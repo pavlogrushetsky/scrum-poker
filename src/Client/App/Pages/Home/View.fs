010790e78af40e4ea3fb8598f121d04eeb6e0b47
Pavlo Hrushetskyi <pavlo.grushetsky@gmail.com>
2020-10-06 23:25:42 +0300

    Refactor Home View to use Feliz.

--- src/Client/App/Pages/Home/View.fs
+++ src/Client/App/Pages/Home/View.fs
@@ -4,6 +4,7 @@ open System
 
 open Fable.React
 open Fable.React.Props
+open Browser.Types
 open Feliz
 
 open Shared
@@ -22,67 +23,106 @@ let private drawStatus connectionState =
     span [ ClassName (sprintf "badge badge-%s" className) ] [ str text ]
 
 let private home' = React.functionComponent("Home", fun ({ Model = model; Dispatch = dispatch }) ->
-    main [ Role "main"; ClassName "container" ] [
-        h3 [] [ str "Send a message!" ]
-        div [ ClassName "row" ] [
-            div [ ClassName "col" ] [
-                div [ ClassName "form-group" ] [
-                    label [ HtmlFor "messageInput"; ClassName "bmd-label-floating" ] [ str "Message" ]
-                    input [ Typeof "text"; ClassName "form-control"; Id "messageInput"; OnChange (fun e -> dispatch(MessageChanged e.Value)) ]
-                ]                    
-            ]
-            div [ ClassName "col-md-auto my-auto" ] [
-                button [ 
-                    Typeof "button"; 
-                    ClassName "btn btn-outline-primary"; 
-                    Disabled (String.IsNullOrEmpty model.MessageToSend || not model.ConnectionState.IsConnected)
-                    OnClick (fun _ -> dispatch (Broadcast (ViaWebSocket, model.MessageToSend))) ] [ str "Broadcast"]
-            ]
-        ]
-        div [ ClassName "row" ] [
-            div [ ClassName "col" ] [ drawStatus model.ConnectionState ]               
-        ]
-        div [ ClassName "row" ] [
-            div [ ClassName "col" ] [ 
-                match model.ReceivedMessages with
-                | [] ->
-                    ()
-                | messages ->
-                    Html.table [
-                        prop.className [ Bs.table; "table-borderless"; Bs.``table-hover`` ]
+    Html.main [
+        prop.role "main"
+        prop.className Bs.container
+        prop.children [
+            Html.h3 [ prop.text "Send a message!" ]
+            Html.div [
+                prop.className Bs.row
+                prop.children [
+                    Html.div [
+                        prop.className Bs.col
                         prop.children [
-                            Html.thead [
-                                prop.className "thead"
+                            Html.div [
+                                prop.className Bs.``form-group``
                                 prop.children [
-                                    Html.tr [
-                                        prop.children [
-                                            Html.th [ 
-                                                prop.scope "col"
-                                                prop.text "Time"
-                                            ]
-                                            Html.th [ 
-                                                prop.scope "col"
-                                                prop.text "Message"
-                                            ]
-                                        ]
+                                    Html.label [
+                                        prop.htmlFor "messageInput"
+                                        prop.className Bs.``bmd-label-floating``
+                                        prop.text "Message"
                                     ]
-                                ]
-                            ]
-                            Html.tbody [
-                                prop.children [
-                                    for message in messages do
-                                        Html.tr [
-                                            prop.children [
-                                                Html.td [ prop.text (sprintf "%O" message.Time) ]
-                                                Html.td [ prop.text message.Text ]
-                                            ]
-                                        ]
+                                    Html.input [
+                                        prop.type' "text"
+                                        prop.className Bs.``form-control``
+                                        prop.id "messageInput"
+                                        prop.onChange (fun (e : Event) -> dispatch(MessageChanged e.Value))
+                                    ]
                                 ]
                             ]
                         ]
                     ]
-            ]               
-        ]                   
+                    Html.div [
+                        prop.className [ Bs.``col-md-auto``; Bs.``my-auto``]
+                        prop.children [
+                            Html.button [
+                                prop.type' "button"
+                                prop.className [ Bs.btn; Bs.``btn-outline-primary`` ]
+                                prop.disabled (String.IsNullOrEmpty model.MessageToSend || not model.ConnectionState.IsConnected)
+                                prop.onClick (fun _ -> dispatch (Broadcast (ViaWebSocket, model.MessageToSend)))
+                                prop.text "Broadcast"
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+            Html.div [
+                prop.className Bs.row
+                prop.children [
+                    Html.div [
+                        prop.className Bs.col
+                        prop.children [ drawStatus model.ConnectionState ]
+                    ]
+                ]
+            ]
+            Html.div [
+                prop.className Bs.row
+                prop.children [
+                    Html.div [
+                        prop.className Bs.col
+                        prop.children [ 
+                            match model.ReceivedMessages with
+                            | [] ->
+                                ()
+                            | messages ->
+                                Html.table [
+                                    prop.className [ Bs.table; "table-borderless"; Bs.``table-hover`` ]
+                                    prop.children [
+                                        Html.thead [
+                                            prop.className "thead"
+                                            prop.children [
+                                                Html.tr [
+                                                    prop.children [
+                                                        Html.th [ 
+                                                            prop.scope "col"
+                                                            prop.text "Time"
+                                                        ]
+                                                        Html.th [ 
+                                                            prop.scope "col"
+                                                            prop.text "Message"
+                                                        ]
+                                                    ]
+                                                ]
+                                            ]
+                                        ]
+                                        Html.tbody [
+                                            prop.children [
+                                                for message in messages do
+                                                    Html.tr [
+                                                        prop.children [
+                                                            Html.td [ prop.text (sprintf "%O" message.Time) ]
+                                                            Html.td [ prop.text message.Text ]
+                                                        ]
+                                                    ]
+                                            ]
+                                        ]
+                                    ]
+                                ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
     ])
 
 let home model dispatch = home' { Model = model; Dispatch = dispatch }
