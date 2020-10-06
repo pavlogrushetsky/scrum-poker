4cc31b006de87f9966306d39b6f1351c5232ba7b
Pavlo Hrushetskyi <pavlo.grushetsky@gmail.com>
2020-10-06 23:52:39 +0300

    Start creating Room page layout.

--- src/Client/App/Pages/Room/View.fs
+++ src/Client/App/Pages/Room/View.fs
@@ -5,11 +5,64 @@ open Fable.React.Props
 open Feliz
 
 open Pages.Room
+open App.Style
 
 let view (model : Model) dispatch =
     Html.h4 [ prop.text model.Title ]
 
 let private room' = React.functionComponent("Room", fun ({ Model = model; Dispatch = dispatch }) ->
-    Html.h4 [ prop.text model.Title ])
+    Html.main [
+        prop.role "main"
+        prop.className Bs.container
+        prop.children [
+            Html.div [
+                prop.className Bs.row
+                prop.children [
+                    Html.div [
+                        prop.className Bs.``col-8``
+                        prop.children [
+                            Html.div [
+                                prop.className Bs.``card-deck``
+                                prop.children [
+                                    Html.div [
+                                        prop.className [ Bs.card; Bs.``bg-info`` ]
+                                        prop.children [
+                                            Html.div [
+                                                prop.className Bs.``card-body``
+                                                prop.children [
+                                                    Html.h1 [
+                                                        prop.className Bs.``card-text``
+                                                        prop.text "0"
+                                                    ]
+                                                ]
+                                            ]
+                                        ]
+                                    ]
+                                    Html.div [
+                                        prop.className Bs.card
+                                    ]
+                                    Html.div [
+                                        prop.className Bs.card
+                                    ]
+                                    Html.div [
+                                        prop.className Bs.card
+                                    ]
+                                    Html.div [
+                                        prop.className Bs.card
+                                    ]
+                                    Html.div [
+                                        prop.className Bs.card
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                    Html.div [
+                        prop.className Bs.``col-4``
+                    ]
+                ]
+            ]
+        ]
+    ])
 
 let room model dispatch = room' { Model = model; Dispatch = dispatch }
