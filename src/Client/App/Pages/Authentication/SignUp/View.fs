7eba9337976f07dbc6d8b8184471cc83803203b5
Pavlo Hrushetskyi <pavlo.grushetsky@gmail.com>
2020-10-10 23:21:48 +0300

    Create SignUpForm component.

--- src/Client/App/Pages/Authentication/SignUp/View.fs
+++ src/Client/App/Pages/Authentication/SignUp/View.fs
@@ -4,6 +4,7 @@ open Feliz
 
 open App.Style
 open App.Routing
+open App.Components.SignUpForm
 open Pages.SignUp
 
 let private signUp' = React.functionComponent("Sign Up", fun ({ Model = model; Dispatch = dispatch }) ->
@@ -29,80 +30,14 @@ let private signUp' = React.functionComponent("Sign Up", fun ({ Model = model; D
                                             ]
                                             Html.div [
                                                 prop.children [
-                                                    Html.form [
-                                                        prop.children [
-                                                            Html.div [
-                                                                prop.className Bs.``form-group``
-                                                                prop.children [
-                                                                    Html.label [
-                                                                        prop.htmlFor "emailInput"
-                                                                        prop.className Bs.``bmd-label-floating``
-                                                                        prop.text "Email"
-                                                                    ]
-                                                                    Html.input [
-                                                                        prop.type' "email"
-                                                                        prop.className Bs.``form-control``
-                                                                        prop.id "emailInput"
-                                                                    ]
-                                                                ]
-                                                            ]
-                                                            Html.div [
-                                                                prop.className Bs.``form-group``
-                                                                prop.children [
-                                                                    Html.label [
-                                                                        prop.htmlFor "passwordInput"
-                                                                        prop.className Bs.``bmd-label-floating``
-                                                                        prop.text "Password"
-                                                                    ]
-                                                                    Html.input [
-                                                                        prop.type' "password"
-                                                                        prop.className Bs.``form-control``
-                                                                        prop.id "passwordInput"
-                                                                    ]
-                                                                ]
-                                                            ]
-                                                            Html.div [
-                                                                prop.className Bs.``form-group``
-                                                                prop.children [
-                                                                    Html.label [
-                                                                        prop.htmlFor "repeatPasswordInput"
-                                                                        prop.className Bs.``bmd-label-floating``
-                                                                        prop.text "Repeat Password"
-                                                                    ]
-                                                                    Html.input [
-                                                                        prop.type' "password"
-                                                                        prop.className Bs.``form-control``
-                                                                        prop.id "repeatPasswordInput"
-                                                                    ]
-                                                                ]
-                                                            ]
-                                                            Html.div [
-                                                                prop.className [ Bs.checkbox; Bs.``my-4`` ]
-                                                                prop.children [
-                                                                    Html.label [                                                                         
-                                                                        prop.children [
-                                                                            Html.input [
-                                                                                prop.type' "checkbox"
-                                                                            ]
-                                                                            Html.text " Remember Me"
-                                                                        ]                                                                                                                                               
-                                                                    ]
-                                                                ]
-                                                            ]
-                                                            Html.button [
-                                                                prop.type' "button"
-                                                                prop.className [ Bs.btn; Bs.``btn-primary``; Bs.``btn-raised``; Bs.``btn-block`` ]
-                                                                prop.text "Sign Up"
-                                                            ]  
-                                                            Html.hr []   
-                                                            Html.a [
-                                                                prop.className [ Bs.btn; Bs.``btn-secondary``; Bs.``btn-block`` ]
-                                                                prop.href (routeHash SignInRoute)
-                                                                prop.onClick goToUrl
-                                                                prop.text "Back to Sign In"
-                                                            ]                                                                                                                                                                                                     
-                                                        ]
-                                                    ]
+                                                    signUpForm ()
+                                                    Html.hr []   
+                                                    Html.a [
+                                                        prop.className [ Bs.btn; Bs.``btn-secondary``; Bs.``btn-block`` ]
+                                                        prop.href (routeHash SignInRoute)
+                                                        prop.onClick goToUrl
+                                                        prop.text "Back to Sign In"
+                                                    ]     
                                                 ]
                                             ]
                                         ]
