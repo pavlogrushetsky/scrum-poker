859c987ad26d576c0080312f990df564a328ab32
Pavlo Hrushetskyi <pavlo.grushetsky@gmail.com>
2020-10-11 22:17:51 +0300

    Rewrite SignUp page to use Semantic UI.

--- src/Client/App/Pages/Authentication/SignUp/View.fs
+++ src/Client/App/Pages/Authentication/SignUp/View.fs
@@ -3,51 +3,14 @@ module Pages.SignUp.View
 open Feliz
 
 open App.Style
-open App.Routing
 open App.Components.SignUpForm
 open Pages.SignUp
 
 let private signUp' = React.functionComponent("Sign Up", fun ({ Model = model; Dispatch = dispatch }) ->
-    Html.main [
-        prop.role "main"
-        prop.className Bs.container
+    Html.div [
+        prop.className [ Sem.ui; Sem.text; Sem.container; Sem.middle; Sem.aligned ]
         prop.children [
-            Html.div [
-                prop.className [ Bs.row; Bs.``justify-content-center`` ]
-                prop.children [
-                    Html.div [
-                        prop.className [ Bs.``col-6`` ]
-                        prop.children [
-                            Html.div [
-                                prop.className [ Bs.card ]
-                                prop.children [
-                                    Html.div [
-                                        prop.className [ Bs.``card-body`` ]
-                                        prop.children [
-                                            Html.h5 [
-                                                prop.className [ Bs.``card-title``; Bs.``text-center`` ]
-                                                prop.text "Sign Up"
-                                            ]
-                                            Html.div [
-                                                prop.children [
-                                                    signUpForm ()
-                                                    Html.hr []   
-                                                    Html.a [
-                                                        prop.className [ Bs.btn; Bs.``btn-secondary``; Bs.``btn-block`` ]
-                                                        prop.href (routeHash SignInRoute)
-                                                        prop.onClick goToUrl
-                                                        prop.text "Back to Sign In"
-                                                    ]     
-                                                ]
-                                            ]
-                                        ]
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]
+            signUpForm ()
         ]
     ])
 
--- src/Client/App/Style.fs
+++ src/Client/App/Style.fs
@@ -7,6 +7,8 @@ open App.Routing
 
 type Bs = CssClasses<"https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css", Naming.Verbatim>     
 
+type Sem = CssClasses<"https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css", Naming.Verbatim>  
+
 type Fa = CssClasses<"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css", Naming.Verbatim>
 
 type prop with
